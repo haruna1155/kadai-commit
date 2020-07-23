b387b5e256b085c11eb776d1e3f0c5e0dd5b639e
haruna <haruharu_1155@gmail.com>
2020-07-23 02:46:49 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>おみくじ</title>
+    </head>
+    <bodey>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：無し</p>
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
+        </form>
+    </bodey>
+</html>
